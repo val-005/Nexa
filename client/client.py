d051bbf33c04c15ee658c6b0fa1ac6d0219084bc
Justin Olivier <91630821+NyFime@users.noreply.github.com>
2025-02-03 15:31:42 +0100

    changement de la structure de client.py

--- client/client.py
+++ client/client.py
@@ -1,41 +1,51 @@
 import socket
 import threading
 
-def receive_message(client_socket: socket.socket) -> None:
-    while True:
-        reponse = client_socket.recv(1024).decode()
-        print(f"{reponse.split(';')[0]}: {reponse.split(';')[1]}")
+class Client:
 
-def main():
-    host = "127.0.0.1"  # Adresse du serveur
-    port = 8001         # Port du serveur
+    def __init__(self, host: str, port: int):
+        self.host = host
+        self.port = port
 
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def receive_message(self, client_socket: socket.socket) -> None:
+        #gère la réception des messages
+        while True:
+            reponse = client_socket.recv(1024).decode()
+            print(f"{reponse.split(';')[0]}: {reponse.split(';')[1]}")
 
-    try:
-        client_socket.connect((host, port))
-    except socket.error as e:
-        print(f"Erreur de connexion au serveur: {e}")
-        return
+    def start(self) -> None:
+        #démarre le client
+        host = self.host  # Adresse du serveur
+        port = self.port         # Port du serveur
 
-    pseudo = input("Entrez votre pseudo : ")
-    registration_msg = f"register;client;{pseudo}"
-    client_socket.send(registration_msg.encode())
-    print("Connecté au serveur !")
-    print("===========================================\n")
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    t = threading.Thread(target=receive_message, args=(client_socket,))
-    t.start()
+        try:
+            client_socket.connect((host, port))
+        except socket.error as e:
+            print(f"Erreur de connexion au serveur: {e}")
+            return
 
-    while True:
-        msg = input("")
-        if msg.lower() == 'quit':
-            break
+        pseudo = input("Entrez votre pseudo : ")
+        registration_msg = f"register;client;{pseudo}"
+        client_socket.send(registration_msg.encode())
+        print("Connecté au serveur !")
+        print("===========================================\n")
 
-        msg_formaté = f"{pseudo};{msg}"
-        client_socket.send(msg_formaté.encode())
-    
-    client_socket.close()
+        #crée un processus pour recevoir les messages
+        threadMsg = threading.Thread(target=self.receive_message, args=(client_socket,))
+        threadMsg.start()
+
+        while True:
+            msg = input("")
+            if msg.lower() == 'quit':
+                break
+
+            msg_formaté = f"{pseudo};{msg}"
+            client_socket.send(msg_formaté.encode())
+        
+        client_socket.close()
 
 if __name__ == "__main__":
-    main()
+    cli = Client('0.0.0.0', 8001)
+    cli.start()
