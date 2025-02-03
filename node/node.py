5a6e4d4a898da41942f7dd4e4202f18276c3b703
Justin Olivier <91630821+NyFime@users.noreply.github.com>
2025-02-03 10:45:05 +0100

    ajout client.py

--- node/node.py
+++ node/node.py
@@ -4,4 +4,10 @@ class Node:
     def __init__(self, host, port):
         self.host = host
         self.port = port
-        
+
+    def connect(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.host, self.port))
+    
+
+node = Node('0.0.0.0', 8088)
