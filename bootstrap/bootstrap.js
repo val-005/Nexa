be5cc76cfe08f83b2150402c988e4efc57cc97d5
val-005 <55240497+val-005@users.noreply.github.com>
2025-02-17 17:16:57 +0100

    Ajout HTTPS

--- bootstrap/bootstrap.js
+++ bootstrap/bootstrap.js
@@ -5,128 +5,139 @@ app.use(express.json());
 const supertest = require("supertest");  // Pour Jest
 const axios = require("axios");
 const sqlite3 = require("sqlite3").verbose();
-
-
 const path = require("path");
-const dbPath = process.env.DB_PATH || "db.sqlite"; 
-// => si DB_PATH n’est pas défini, fallback sur "db.sqlite"
+const fs = require("fs");
+const https = require("https");
 
-const fullPath = path.resolve(dbPath); 
-// => résout le chemin absolu pour éviter les surprises
-
+const dbPath = process.env.DB_PATH || "db.sqlite";
+const fullPath = path.resolve(dbPath);
 const db = new sqlite3.Database(fullPath);
 db.serialize(() => {
-    db.run("CREATE TABLE IF NOT EXISTS nodes (id INTEGER PRIMARY KEY AUTOINCREMENT, node TEXT UNIQUE)");
-    db.run("CREATE TABLE IF NOT EXISTS upnodes (id INTEGER PRIMARY KEY AUTOINCREMENT, node TEXT UNIQUE)");
+  db.run("CREATE TABLE IF NOT EXISTS nodes (id INTEGER PRIMARY KEY AUTOINCREMENT, node TEXT UNIQUE)");
+  db.run("CREATE TABLE IF NOT EXISTS upnodes (id INTEGER PRIMARY KEY AUTOINCREMENT, node TEXT UNIQUE)");
 });
 
-// Partie Fonctions
+const PROTOCOL = process.env.NODE_PROTOCOL || 'http';
+
 const checkNodes = async () => {
-    db.all("SELECT node FROM nodes", async (err, table) => {
-        if (err) {
-            console.log(err);
-        }
-    const upNodes = [];
-    for (const col of table) {
-        try {
-            // Si le noeud répond correctement à la requete get, alors on l'ajoute a la liste des noeuds up
-            const response = await axios.get(`http://${col.node}/status`);
-            if (response.status === 200) {
-                upNodes.push(col.node);
-            }
-        } catch (error) {
-        }
+  db.all("SELECT node FROM nodes", async (err, table) => {
+    if (err) {
+      console.log(err);
+      return; 
     }
 
-
-    db.run("DELETE FROM upnodes", (err) => {
-        if (err) {
-            console.log(err);
-        } else {
-            if (upNodes.length > 0) {
-                const placeholders = upNodes.map(() => "(?)").join(",");
-                db.run(`INSERT INTO upnodes (node) VALUES ${placeholders}`, upNodes, (err) => {
-                    if (err) {
-                        console.log(err);
-                    }
-                });
-            }
+    const upNodes = [];
+    for (const col of table) {
+      try {
+        const response = await axios.get(`http://${col.node}:8080/status`, {
+        });
+        if (response.status === 200) {
+          upNodes.push(col.node);
         }
+      } catch (error) {
+      }
+    }
+
+    db.run("DELETE FROM upnodes", (err2) => {
+      if (err2) {
+        console.log(err2);
+      } else {
+        if (upNodes.length > 0) {
+          const placeholders = upNodes.map(() => "(?)").join(",");
+          db.run(`INSERT INTO upnodes (node) VALUES ${placeholders}`, upNodes, (err3) => {
+            if (err3) {
+              console.log(err3);
+            }
+          });
+        }
+      }
     });
     console.log("Up Nodes:", upNodes);
-});
-}
+  });
+};
 checkNodes();
 const checkNodesInterval = setInterval(checkNodes, 5 * 60 * 1000);
 
 
 app.get("/status", (req, res) => {
-    const status = {
-        status: "Server is running",
-        current_time: new Date().toLocaleString()
-    };
-
-res.send(status);
-
+  const status = {
+    status: "Server is running",
+    current_time: new Date().toLocaleString()
+  };
+  res.send(status);
 });
 
-// Fonction pour récupérer la liste des noeuds enregistrés, a modifier / supprimer car inutile
 app.get("/nodes", (req, res) => {
-    db.all("SELECT node FROM nodes", (err, table) => {
-        if (err) {
-            res.status(500).send({ error: "Failed to retrieve nodes" });
-        } else {
-            const nodes = table.map(col => col.node);
-            res.send(nodes);
-        }
-    });
+  db.all("SELECT node FROM nodes", (err, table) => {
+    if (err) {
+      res.status(500).send({ error: "Failed to retrieve nodes" });
+    } else {
+      const nodes = table.map(col => col.node);
+      res.send(nodes);
+    }
+  });
 });
 
 app.post("/registerNode", (req, res) => {
-    let { node } = req.body;
-    node = node.replace(/^http:\/\//, '').replace(/\/$/, '');
+  let { node } = req.body;
 
-    const regex = /^[a-zA-Z0-9.-]+:[0-9]+$/;  // Format host:port
-    if (!regex.test(node)) {
-        return res.status(400).send({ error: "Invalid node format. Use 'host:port'." });
+  node = node.replace(/^http:\/\//, '').replace(/\/$/, '');
+
+  const regex = /^[a-zA-Z0-9.-]+:[0-9]+$/;  // Format host:port
+  if (!regex.test(node)) {
+    return res.status(400).send({ error: "Invalid node format. Use 'host:port'." });
+  }
+
+  db.get(`SELECT node FROM nodes WHERE node = ?`, [node], (err, col) => {
+    if (err) {
+      return res.status(500).send({ error: "Database error" });
+    }
+    if (col) {
+      return res.status(400).send({ error: "Node is already registered" });
     }
 
-    db.get(`SELECT node FROM nodes WHERE node = ?`, [node], (err, col) => {
-        if (err) {
-            return res.status(500).send({ error: "Database error" });
+    db.run(`INSERT INTO nodes (node) VALUES (?)`, [node], (err2) => {
+      if (err2) {
+        if (err2.code === 'SQLITE_CONSTRAINT') {
+          return res.status(400).send({ error: "Node is already registered" });
         }
-        if (col) {
-            return res.status(400).send({ error: "Node is already registered" });
-        }
-
-        db.run(`INSERT INTO nodes (node) VALUES (?)`, [node], (err) => {
-            if (err) {
-                if (err.code === 'SQLITE_CONSTRAINT') {
-                    return res.status(400).send({ error: "Node is already registered" });
-                }
-                return res.status(500).send({ error: "Failed to register node" });
-            }
-            res.send({ status: "success" });
-        });
+        return res.status(500).send({ error: "Failed to register node" });
+      }
+      res.send({ status: "success" });
     });
+  });
 });
 
-
-// Récupérer la liste des noeuds up
 app.get("/upNodes", async (req, res) => {
-    db.all("SELECT node FROM upnodes", (err, table) => {
-        if (err) {
-            res.status(500).send({ error: "Failed to retrieve nodes" });
-        } else {
-            const nodes = table.map(col => col.node);
-            res.send(nodes);
-        }
-    });
-})
+  db.all("SELECT node FROM upnodes", (err, table) => {
+    if (err) {
+      res.status(500).send({ error: "Failed to retrieve nodes" });
+    } else {
+      const nodes = table.map(col => col.node);
+      res.send(nodes);
+    }
+  });
+});
+
+const SSL_ENABLED = process.env.SSL_ENABLED === "true";
+const PORT = process.env.PORT || (SSL_ENABLED ? 443 : 80);
+const KEY_PATH = process.env.SSL_KEY_PATH;
+const CRT_PATH = process.env.SSL_CRT_PATH;
 
 if (require.main === module) {
-    const PORT = process.env.PORT || 80;
-    app.listen(PORT, () => console.log("Server Listening on PORT:", PORT));
-}
-
+    if (SSL_ENABLED) {
+      const privateKey  = fs.readFileSync(KEY_PATH, 'utf8');
+      const certificate = fs.readFileSync(CRT_PATH, 'utf8');
+  
+      const credentials = { key: privateKey, cert: certificate, ca: ca };
+      https.createServer(credentials, app).listen(PORT, () => {
+        console.log("HTTPS Server listening on PORT:", PORT);
+      });
+    } else {
+      app.listen(PORT, () => {
+        console.log("HTTP Server Listening on PORT:", PORT);
+      });
+    }
+  }
+ 
 module.exports = { app, checkNodesInterval };
